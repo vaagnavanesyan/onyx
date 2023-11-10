4c28aa078f9f5ee8f632f890ecc5de7aab613e72
Vaagn Avanesyan <vavanesyan@adsterra.com>
2023-11-10 17:54:28 +0300

    fix: фильтруем воду

--- kitchen/espresso/src/lib/espresso.spec.ts
+++ kitchen/espresso/src/lib/espresso.spec.ts
@@ -2,6 +2,6 @@ import { espresso } from './espresso';
 
 describe('espresso', () => {
   it('should work', () => {
-    expect(espresso()).toEqual('[зерна (обжаренные) (молотые) + вода (горячая)]');
+    expect(espresso()).toEqual('[зерна (обжаренные) (молотые) + вода (горячая) (фильтрованная)]');
   });
 });
